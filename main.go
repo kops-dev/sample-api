75d434dbd189563f1e3c6ec5452679af9e8c6764
surajit-zs <surajit.pore@kops.dev>
2024-07-05 12:41:20 +0530

    update gofr and update endpoint

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	"gofr.dev/pkg/gofr"
 )
 
@@ -9,33 +10,16 @@ func main() {
 	app := gofr.New()
 
 	app.GET("/hello", func(c *gofr.Context) (interface{}, error) {
-		return fmt.Sprintf("Hello! Welcome to %s", c.GetAppName()), nil
-	})
-
-	app.GET("/error", func(c *gofr.Context) (interface{}, error) {
-		return nil, fmt.Errorf("sample error")
-	})
-
-	app.GET("/warn", func(c *gofr.Context) (interface{}, error) {
+		c.Logger.Debug("sample debug log")
+		c.Logger.Info("sample info log")
 		c.Logger.Warn("sample warn log")
+		c.Logger.Errorf("sample error log")
 
-		return nil, nil
+		return fmt.Sprintf("Hello! Welcome to %s", c.GetAppName()), nil
 	})
 
 	app.GET("/fatal", func(c *gofr.Context) (interface{}, error) {
 		c.Logger.Fatal("sample warn log")
-
-		return nil, nil
-	})
-
-	app.GET("/debug", func(c *gofr.Context) (interface{}, error) {
-		c.Logger.Debug("sample debug log")
-
-		return nil, nil
-	})
-
-	app.GET("/info", func(c *gofr.Context) (interface{}, error) {
-		c.Logger.Info("sample info log")
 
 		return nil, nil
 	})
