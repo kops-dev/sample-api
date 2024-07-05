83426c85416e346abf0b25b6e16db397dabbe78b
surajit-zs <surajit.pore@kops.dev>
2024-07-05 12:43:33 +0530

    refactor code

--- main.go
+++ main.go
@@ -10,12 +10,17 @@ func main() {
 	app := gofr.New()
 
 	app.GET("/hello", func(c *gofr.Context) (interface{}, error) {
+
+		return fmt.Sprintf("Hello! Welcome to %s", c.GetAppName()), nil
+	})
+
+	app.GET("/logs", func(c *gofr.Context) (interface{}, error) {
 		c.Logger.Debug("sample debug log")
 		c.Logger.Info("sample info log")
 		c.Logger.Warn("sample warn log")
 		c.Logger.Errorf("sample error log")
 
-		return fmt.Sprintf("Hello! Welcome to %s", c.GetAppName()), nil
+		return "success", nil
 	})
 
 	app.GET("/fatal", func(c *gofr.Context) (interface{}, error) {
