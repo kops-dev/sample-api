e620f88c309fad56da5391e634f9ae897b41c790
Surajit Pore <97510117+surajit-zs@users.noreply.github.com>
2024-07-05 12:16:00 +0530

    add specific endpoint for error, warn, fatal, info (#2)

--- main.go
+++ main.go
@@ -12,5 +12,33 @@ func main() {
 		return fmt.Sprintf("Hello! Welcome to %s", c.GetAppName()), nil
 	})
 
+	app.GET("/error", func(c *gofr.Context) (interface{}, error) {
+		return nil, fmt.Errorf("sample error")
+	})
+
+	app.GET("/warn", func(c *gofr.Context) (interface{}, error) {
+		c.Logger.Warn("sample warn log")
+
+		return nil, nil
+	})
+
+	app.GET("/fatal", func(c *gofr.Context) (interface{}, error) {
+		c.Logger.Fatal("sample warn log")
+
+		return nil, nil
+	})
+
+	app.GET("/debug", func(c *gofr.Context) (interface{}, error) {
+		c.Logger.Debug("sample debug log")
+
+		return nil, nil
+	})
+
+	app.GET("/info", func(c *gofr.Context) (interface{}, error) {
+		c.Logger.Info("sample info log")
+
+		return nil, nil
+	})
+
 	app.Run()
 }
